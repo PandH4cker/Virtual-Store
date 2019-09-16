87fe807d81d0301443f3aade794e5af3b86fec3c
Raphael Dray <dray.raph@gmail.com>
2019-09-16 11:54:58 +0200

    Add: Creating First Test with JUnit 5

--- src/store/business/test/VirtualStoreTestSuite.java
+++ src/store/business/test/VirtualStoreTestSuite.java
@@ -1,5 +1,19 @@
 package store.business.test;
 
 
+import org.junit.jupiter.api.Test;
+import store.business.util.product.Product;
+import store.business.util.product.VideoGame;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class VirtualStoreTestSuite {
+    private final Product jeux = new VideoGame("Borderlands", "60",
+                                                "14203148", "100",
+                                                "files/images/CallOfDutyMW.jpg", "Multi-joueurs",
+                                                "XBOX ONE X");
+    @Test
+    void createVideoGameAndPrintIt() {
+        assertEquals("titre: Borderlands, genre: Multi-joueurs, platforme: XBOX ONE X", jeux.toString());
+    }
 }
